a6599957bb6aa5986f09e404bf9284075a46f550
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-12-15 16:48:36 +0100

    media query and tab done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "../sass/main.scss";
 
+const navigation = document.querySelector(".navigation");
 const navbarToggler = document.getElementById("navbar-toggler");
 const togglerIcon = document.querySelector(".toggler-icon");
 const navigationNav = document.getElementsByClassName("navigation__nav")[0];
@@ -9,7 +10,15 @@ navbarToggler.addEventListener("click", () => {
     togglerIcon.src = "img/burger.svg";
     navigationNav.style.display = "none";
   } else {
+    navigation.style.backgroundColor =  "rgba(0, 0, 0, 0.25)";
     togglerIcon.src = "img/left-arrow.svg";
     navigationNav.style.display = "block";
   }
 });
+
+window.addEventListener('resize', (e) => {
+  if (window.innerWidth >= 1200) {
+    togglerIcon.src = "img/burger.svg";
+    navigationNav.style.display = "none";
+  }
+})
