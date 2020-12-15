3b0adcaf25446ded0038c6ad18b4b183cb0d6370
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-12-15 18:05:21 +0100

    some fix

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import "../sass/main.scss";
 
 const navigation = document.querySelector(".navigation");
-const navbarToggler = document.getElementById("navbar-toggler");
-const togglerIcon = document.querySelector(".toggler-icon");
-const navigationNav = document.getElementsByClassName("navigation__nav")[0];
+const navbarToggler = document.querySelector(".navigation__toggle");
+const navigationNav = document.querySelector(".navigation__nav");
+const swapingIcon = document.querySelector(".toggle-icon");
 
 navbarToggler.addEventListener("click", () => {
   if (navigationNav.style.display === "block") {
-    togglerIcon.src = "img/burger.svg";
+    swapingIcon.src = "img/burger.svg";
     navigationNav.style.display = "none";
   } else {
-    navigation.style.backgroundColor =  "rgba(0, 0, 0, 0.25)";
-    togglerIcon.src = "img/left-arrow.svg";
+    navigation.style.backgroundColor = "rgba(0, 0, 0, 0.25)";
+    swapingIcon.src = "img/left-arrow.svg";
     navigationNav.style.display = "block";
   }
 });
 
-window.addEventListener('resize', (e) => {
-  if (window.innerWidth >= 1200) {
-    togglerIcon.src = "img/burger.svg";
-    navigationNav.style.display = "none";
+window.addEventListener("resize", (e) => {
+  if (window.innerWidth >= 900) {
+    if ((navigationNav.style.display = "block")) {
+      navigationNav.style.display = "none";
+    }
   }
-})
+});
