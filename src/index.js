8f5c3510f85d60beb9360e4d9c2ae70edeac060c
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-12-18 16:43:19 +0100

    webpack update configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "../sass/main.scss";
+import './img/logo.svg';
+import './img/burger.svg';
+import './img/left-arrow.svg';
 
 const navigation = document.querySelector(".navigation");
 
@@ -8,18 +11,19 @@ const swapingIcon = document.querySelector(".toggle-icon");
 const navigationNav = document.querySelector(".navigation__nav");
 const navigationList = document.querySelector(".navigation__list");
 const navigationItems = document.querySelectorAll(".navigation__item");
-const navigationListDesktop = document.querySelector(".navigation__list-desktop");
-
+const navigationListDesktop = document.querySelector(
+  ".navigation__list-desktop"
+);
 
 navigationToggle.addEventListener("click", () => {
   if (navigationNav.style.display === "block") {
-    swapingIcon.src = "img/burger.svg";
+    swapingIcon.src = "./img/burger.svg";
     navigationNav.style.display = "none";
     navigationToggle.setAttribute("aria-expanded", "false");
   } else {
     navigationToggle.setAttribute("aria-expanded", "true");
     navigation.style.backgroundColor = "rgba(0, 0, 0, 0.25)";
-    swapingIcon.src = "img/left-arrow.svg";
+    swapingIcon.src = "./img/left-arrow.svg";
     navigationNav.style.display = "block";
   }
 });
@@ -27,7 +31,7 @@ navigationToggle.addEventListener("click", () => {
 for (const item of navigationItems) {
   item.addEventListener("click", () => {
     navigationNav.style.display = "none";
-    swapingIcon.src = "img/burger.svg";
+    swapingIcon.src = "./img/burger.svg";
   });
 }
 
@@ -39,7 +43,7 @@ window.onload = function () {
 
 window.addEventListener("resize", (e) => {
   if (window.innerWidth >= 900) {
-    swapingIcon.src = "img/burger.svg";
+    swapingIcon.src = "./img/burger.svg";
     navigationListDesktop.appendChild(navigationList);
     navigation.style.backgroundColor = "#404040";
     navigationNav.style.display = "none";
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,48 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 
 module.exports = {
-    entry: ['./src/index.js'],
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'index.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            "style-loader",
-            "css-loader",
-            "sass-loader",
-          ],
+  mode: mode,
+  devServer: {
+    open: true,
+  },
+  watch: true,
+  devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "img"
         },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: ["babel-loader"]
-        }
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin()
-    ]
-  };
-  
-  
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+};
